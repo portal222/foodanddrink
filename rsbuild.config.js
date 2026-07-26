7bf2963fed297e56c818411982a61a0d4a52e1a3
portal222 <dvaportala@gmail.com>
2026-07-26 18:59:50 +0200

    redirects in public second time

--- rsbuild.config.js
+++ rsbuild.config.js
@@ -1,6 +1,17 @@
 const { defineConfig } = require('@rsbuild/core');
 const { pluginSass } = require('@rsbuild/plugin-sass');
 
+const path = require('path');
+
 module.exports = defineConfig({
   plugins: [pluginSass()],
+  output: {
+    // Kopiraj statičke fajlove iz public u dist
+    copy: [
+      { from: path.resolve(__dirname, 'public'), to: path.resolve(__dirname, 'dist') },
+    ],
+  },
 });
+
+
+
